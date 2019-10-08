d3d6ed4260456b5c41dda23185412ac4b6654c39
Rafael Camargo <hello@rafaelcamargo.com>
2019-10-08 12:52:28 -0300

    chore: create project config

---
+++ jest.config.js
@@ -0,0 +1,26 @@
+const project = require('./project.json');
+
+module.exports = {
+  collectCoverageFrom: project.tests.source.files,
+  coverageReporters: ['html'],
+  coverageThreshold: {
+    global: {
+      statements: 100,
+      branches: 100,
+      functions: 100,
+      lines: 100
+    }
+  },
+  moduleNameMapper: {
+    '@environment$': `<rootDir>/${project.environments.source.root}/development.js`,
+    '@scripts\/(.*)$': `<rootDir>/${project.scripts.source.root}$1`,
+    '@styles\/(.*)$': `<rootDir>/${project.styles.source.root}$1`,
+    '@images\/(.*)$': `<rootDir>/${project.images.source.root}$1`,
+    '^.+\\.css$': '<rootDir>/src/scripts/base/mocks/raw-files.js'
+  },
+  setupFilesAfterEnv: ['<rootDir>/src/scripts/base/mocks/global.js'],
+  transform: {
+    '^.+\\.(png|styl)$': '<rootDir>/src/scripts/base/mocks/raw-files.js',
+    '^.+\\.js$': 'babel-jest'
+  }
+};
