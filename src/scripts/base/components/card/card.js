98b2334c466cc2370a05a1ba9fc12937442f0b19
Rafael Camargo <hello@rafaelcamargo.com>
2019-10-10 11:27:35 -0300

    feat: add title and fetch props to card

--- src/scripts/base/components/card/card.js
+++ src/scripts/base/components/card/card.js
@@ -1,12 +1,49 @@
 import '@styles/card.styl';
 import React, { Component } from 'react';
+import { WLoader } from '@scripts/base/components/loader/loader';
 
 export class WCard extends Component {
+  constructor(props){
+    super(props);
+    this.state = {};
+  }
+  componentDidMount(){
+    if(this.props.onFetch)
+      this.fetch();
+  }
+  fetch(){
+    this.setLoaderVisibility(true);
+    this.props.onFetch().then(response => {
+      this.props.onFetchSuccess(response);
+      this.onFetchComplete();
+    }, err => {
+      this.props.onFetchError(err);
+      this.onFetchComplete();
+    });
+  }
+  onFetchComplete(){
+    this.setLoaderVisibility(false);
+  }
+  setLoaderVisibility(shouldShow){
+    this.setState({ shouldShowLoader: shouldShow });
+  }
   render() {
     return (
       <div className="w-card">
-        { this.props.children }
+        { buildLoader(this.state.shouldShowLoader) }
+        { buildTitle(this.props.title) }
+        <div className="w-card-content">
+          { this.props.children }
+        </div>
       </div>
     );
   }
+}
+
+function buildLoader(shouldShow){
+  return shouldShow ? <div className="w-card-loader"><WLoader /></div> : null;
+}
+
+function buildTitle(title){
+  return title ? <div className="w-card-title" data-card-title>{ title }</div> : null;
 }
--- src/scripts/base/components/card/card.test.js
+++ src/scripts/base/components/card/card.test.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+import { PromiseMock } from '@scripts/base/mocks/promise';
+import { WLoader } from '@scripts/base/components/loader/loader';
 import { WCard } from '@scripts/base/components/card/card';
 
 describe('Card', () => {
   function mount(props = {}){
     return shallow(
-      <WCard>
+      <WCard
+        title={ props.title }
+        onFetch={ props.onFetch }
+        onFetchSuccess={ props.onFetchSuccess }
+        onFetchError={ props.onFetchError }>
         { props.content }
       </WCard>
     );
@@ -14,6 +20,42 @@ describe('Card', () => {
   it('should have appropriate css class', () => {
     const wrapper = mount();
     expect(wrapper.prop('className')).toEqual('w-card');
+  });
+
+  it('should optionally render a title', () => {
+    const title = 'Card';
+    const wrapper = mount({ title });
+    expect(wrapper.find('[data-card-title]').text()).toEqual(title);
+  });
+
+  it('should execute fetch callback if callback has been given', () => {
+    const onFetch = jest.fn(() => new PromiseMock('success', { shouldAbortRequest: true }));
+    const wrapper = mount({ onFetch });
+    expect(onFetch).toHaveBeenCalled();
+  });
+
+  it('should show loader on fetch', () => {
+    const onFetch = jest.fn(() => new PromiseMock('success', { shouldAbortRequest: true }));
+    const wrapper = mount({ onFetch });
+    expect(wrapper.find(WLoader).length).toEqual(1);
+  });
+
+  it('should execute fetch success callback on fetch success', () => {
+    const response = { some: 'response' };
+    const onFetch = jest.fn(() => new PromiseMock('success', { response }));
+    const onFetchSuccess = jest.fn();
+    const wrapper = mount({ onFetch, onFetchSuccess });
+    expect(onFetchSuccess).toHaveBeenCalledWith(response);
+    expect(wrapper.find(WLoader).length).toEqual(0);
+  });
+
+  it('should execute fetch error callback on fetch error', () => {
+    const err = { some: 'err' };
+    const onFetch = jest.fn(() => new PromiseMock('error', { err }));
+    const onFetchError = jest.fn();
+    const wrapper = mount({ onFetch, onFetchError });
+    expect(onFetchError).toHaveBeenCalledWith(err);
+    expect(wrapper.find(WLoader).length).toEqual(0);
   });
 
   it('should render some content', () => {
