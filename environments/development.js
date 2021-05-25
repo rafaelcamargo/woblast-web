c27d6add5b978f46094e25be73620d3d26b81ea4
Rafael Camargo <hello@rafaelcamargo.com>
2021-05-24 23:59:02 -0300

    fix: finance api resource

--- environments/development.js
+++ environments/development.js
@@ -1,8 +1,11 @@
 module.exports = {
-  HG_BRASIL_API: {
-    BASE_URL: 'http://localhost:3000',
-    FORMAT: 'json',
-    KEY: '489e9445'
+  VERVET: {
+    BASE_URL: 'http://localhost:9001',
+    PROJECTS: {
+      FINANCE: {
+        ID: 'e21cbad5-1613-4568-ac8a-6f720ceffd4a'
+      }
+    }
   },
   MONITOR: {
     INTERVAL: 5000,
